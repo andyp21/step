a9ecd3be1c3fbddc3074628e27ef9a6b2ef7a49f
Andrew <andypauul@gmail.com>
2020-07-01 19:03:29 +0000

    - take out unnecessary libraries

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet_rev.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet_rev.java
@@ -19,8 +19,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList; 
-import com.google.gson.Gson;
 
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
