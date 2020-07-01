5e0e2c0681985687a47aee125cf4afff9ec85d3d
Andrew <andypauul@gmail.com>
2020-07-01 18:58:42 +0000

    Step 4 POST - create a new Java Servlet file DataServlet_rev.java - implement form which reverses the text input

---
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet_rev.java
@@ -0,0 +1,57 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList; 
+import com.google.gson.Gson;
+
+
+/** Servlet that returns some example content. TODO: modify this file to handle comments data */
+@WebServlet("/reverse")
+public class DataServlet_rev extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get the input from the form.
+    String text = getParameter(request, "text-input", "");
+    
+    // Reverses String text
+    StringBuilder text_rev = new StringBuilder(); 
+    text_rev.append(text); 
+    text_rev = text_rev.reverse(); 
+
+    // Respond with the result.
+    response.setContentType("text/html;");
+    response.getWriter().println(text_rev);
+  }
+
+
+  /**
+   * @return the request parameter, or the default value if the parameter
+   *         was not specified by the client
+   */
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
+}
